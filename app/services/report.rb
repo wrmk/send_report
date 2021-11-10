d5547fc05ed6080098455fd285b414b8ea13bf71
wrmk <elmakan@gmail.com>
2021-11-10 13:07:23 +0300

    refactor

--- app/services/report.rb
+++ app/services/report.rb
@@ -1,12 +1,12 @@
 class Report
   class << self
     def generate(data)
-      report = data.reduce(''){|message, hash| message << generate_string(hash)}
+      data.reduce(''){|report, hash| report << generate_string(hash)}
     end
 
     private
     def generate_string(hash)
-      string = "#{hash[:code]}. Guest: #{hash[:guest]}. #{hash[:type].capitalize} #{hash[:entity]} at '#{hash[:created_at]}' \n"
+      "#{hash[:code]}. Guest: #{hash[:guest]}. #{hash[:type].capitalize} #{hash[:entity]} at '#{hash[:created_at]}'\n"
     end
   end
 end
