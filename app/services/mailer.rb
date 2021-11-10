809107fdb82df68d7cb7bdf0c8864f408bad8fd5
wrmk <elmakan@gmail.com>
2021-11-10 13:10:02 +0300

    mailer service

---
+++ app/services/mailer.rb
@@ -0,0 +1,15 @@
+class Mailer
+  #
+  # Send report by email
+  #
+  # @param [String] from
+  # @param [String] to
+  # @param [String] subject
+  # @param [String] report
+  #
+  # @return [Boolean]
+  #
+  def self.deliver(from:, to:, subject:, body:)
+    # ...
+  end
+end
