2ad982abdf3067a356ee7055ba7b519af75bb713
wrmk <elmakan@gmail.com>
2021-11-10 16:48:55 +0300

    refactor

--- spec/services/report_spec.rb
+++ spec/services/report_spec.rb
@@ -5,9 +5,9 @@ data = [
   { code: 'A-001', guest: 'guest@email.com', entity: 'reservation', type: 'modified', created_at: '2019-06-08 23:40:02'}
 ]
 
-report = <<here 
-A-001. Guest: guest@email.com. Confirmed reservation at '2019-06-08 23:06:45'
-A-001. Guest: guest@email.com. Modified reservation at '2019-06-08 23:40:02'
+report = <<~here 
+  A-001. Guest: guest@email.com. Confirmed reservation at '2019-06-08 23:06:45'
+  A-001. Guest: guest@email.com. Modified reservation at '2019-06-08 23:40:02'
 here
 
 RSpec.describe Report, type: :model do
